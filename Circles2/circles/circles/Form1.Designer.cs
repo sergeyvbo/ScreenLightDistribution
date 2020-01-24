86dfa8cd770214e5b2a62ac3a8b54e084ac2051a
Sergey Bogachev <47236070+sergeyvbo@users.noreply.github.com>
2020-01-24 13:29:19 +0300

    new repo

---
+++ Circles2/circles/circles/Form1.Designer.cs
@@ -0,0 +1,333 @@
+﻿namespace circles
+{
+    partial class Circles
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            var resources = new System.ComponentModel.ComponentResourceManager(typeof(Circles));
+            this.panelTools = new System.Windows.Forms.Panel();
+            this.buttonRefresh = new System.Windows.Forms.Button();
+            this.numericUpDownIncrement = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownRadius = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownInterval = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownHeight = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownWidth = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelScale = new System.Windows.Forms.Label();
+            this.numericUpDownScale = new System.Windows.Forms.NumericUpDown();
+            this.panelWorkSpace = new System.Windows.Forms.Panel();
+            this.panelDrawBox = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panelTools.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownIncrement)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownRadius)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownInterval)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownHeight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownWidth)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownScale)).BeginInit();
+            this.panelWorkSpace.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelTools
+            // 
+            this.panelTools.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelTools.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelTools.Controls.Add(this.button1);
+            this.panelTools.Controls.Add(this.buttonRefresh);
+            this.panelTools.Controls.Add(this.numericUpDownIncrement);
+            this.panelTools.Controls.Add(this.numericUpDownRadius);
+            this.panelTools.Controls.Add(this.numericUpDownInterval);
+            this.panelTools.Controls.Add(this.numericUpDownHeight);
+            this.panelTools.Controls.Add(this.numericUpDownWidth);
+            this.panelTools.Controls.Add(this.label5);
+            this.panelTools.Controls.Add(this.label4);
+            this.panelTools.Controls.Add(this.label3);
+            this.panelTools.Controls.Add(this.label2);
+            this.panelTools.Controls.Add(this.label1);
+            this.panelTools.Location = new System.Drawing.Point(1, 0);
+            this.panelTools.Name = "panelTools";
+            this.panelTools.Size = new System.Drawing.Size(682, 29);
+            this.panelTools.TabIndex = 0;
+            // 
+            // buttonRefresh
+            // 
+            this.buttonRefresh.Location = new System.Drawing.Point(555, 2);
+            this.buttonRefresh.Name = "buttonRefresh";
+            this.buttonRefresh.Size = new System.Drawing.Size(75, 23);
+            this.buttonRefresh.TabIndex = 6;
+            this.buttonRefresh.Text = "Обновить";
+            this.buttonRefresh.UseVisualStyleBackColor = true;
+            this.buttonRefresh.Click += new System.EventHandler(this.buttonRefresh_Click);
+            // 
+            // numericUpDownIncrement
+            // 
+            this.numericUpDownIncrement.DecimalPlaces = 2;
+            this.numericUpDownIncrement.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.numericUpDownIncrement.Location = new System.Drawing.Point(502, 3);
+            this.numericUpDownIncrement.Maximum = new decimal(new int[] {
+            9999,
+            0,
+            0,
+            0});
+            this.numericUpDownIncrement.Minimum = new decimal(new int[] {
+            9999,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDownIncrement.Name = "numericUpDownIncrement";
+            this.numericUpDownIncrement.Size = new System.Drawing.Size(47, 20);
+            this.numericUpDownIncrement.TabIndex = 5;
+            this.numericUpDownIncrement.ValueChanged += new System.EventHandler(this.OnValueChanged);
+            // 
+            // numericUpDownRadius
+            // 
+            this.numericUpDownRadius.Location = new System.Drawing.Point(373, 3);
+            this.numericUpDownRadius.Maximum = new decimal(new int[] {
+            9999,
+            0,
+            0,
+            0});
+            this.numericUpDownRadius.Name = "numericUpDownRadius";
+            this.numericUpDownRadius.Size = new System.Drawing.Size(47, 20);
+            this.numericUpDownRadius.TabIndex = 5;
+            this.numericUpDownRadius.ValueChanged += new System.EventHandler(this.OnValueChanged);
+            // 
+            // numericUpDownInterval
+            // 
+            this.numericUpDownInterval.Location = new System.Drawing.Point(273, 3);
+            this.numericUpDownInterval.Maximum = new decimal(new int[] {
+            9999,
+            0,
+            0,
+            0});
+            this.numericUpDownInterval.Name = "numericUpDownInterval";
+            this.numericUpDownInterval.Size = new System.Drawing.Size(47, 20);
+            this.numericUpDownInterval.TabIndex = 5;
+            this.numericUpDownInterval.ValueChanged += new System.EventHandler(this.OnValueChanged);
+            // 
+            // numericUpDownHeight
+            // 
+            this.numericUpDownHeight.Location = new System.Drawing.Point(158, 3);
+            this.numericUpDownHeight.Maximum = new decimal(new int[] {
+            9999,
+            0,
+            0,
+            0});
+            this.numericUpDownHeight.Name = "numericUpDownHeight";
+            this.numericUpDownHeight.Size = new System.Drawing.Size(47, 20);
+            this.numericUpDownHeight.TabIndex = 5;
+            this.numericUpDownHeight.ValueChanged += new System.EventHandler(this.OnValueChanged);
+            // 
+            // numericUpDownWidth
+            // 
+            this.numericUpDownWidth.Location = new System.Drawing.Point(52, 3);
+            this.numericUpDownWidth.Maximum = new decimal(new int[] {
+            9999,
+            0,
+            0,
+            0});
+            this.numericUpDownWidth.Name = "numericUpDownWidth";
+            this.numericUpDownWidth.Size = new System.Drawing.Size(47, 20);
+            this.numericUpDownWidth.TabIndex = 5;
+            this.numericUpDownWidth.ValueChanged += new System.EventHandler(this.OnValueChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(428, 7);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Приращение:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(328, 7);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Радиус:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(215, 7);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Интервал:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(111, 7);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Высота:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ширина:";
+            // 
+            // labelScale
+            // 
+            this.labelScale.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelScale.AutoSize = true;
+            this.labelScale.Location = new System.Drawing.Point(567, 468);
+            this.labelScale.Name = "labelScale";
+            this.labelScale.Size = new System.Drawing.Size(56, 13);
+            this.labelScale.TabIndex = 2;
+            this.labelScale.Text = "Масштаб:";
+            // 
+            // numericUpDownScale
+            // 
+            this.numericUpDownScale.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDownScale.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDownScale.Location = new System.Drawing.Point(625, 465);
+            this.numericUpDownScale.Maximum = new decimal(new int[] {
+            400,
+            0,
+            0,
+            0});
+            this.numericUpDownScale.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownScale.Name = "numericUpDownScale";
+            this.numericUpDownScale.Size = new System.Drawing.Size(53, 20);
+            this.numericUpDownScale.TabIndex = 3;
+            this.numericUpDownScale.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownScale.ValueChanged += new System.EventHandler(this.OnValueChanged);
+            // 
+            // panelWorkSpace
+            // 
+            this.panelWorkSpace.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelWorkSpace.AutoScroll = true;
+            this.panelWorkSpace.BackColor = System.Drawing.Color.Black;
+            this.panelWorkSpace.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panelWorkSpace.Controls.Add(this.panelDrawBox);
+            this.panelWorkSpace.Location = new System.Drawing.Point(1, 29);
+            this.panelWorkSpace.Name = "panelWorkSpace";
+            this.panelWorkSpace.Size = new System.Drawing.Size(682, 434);
+            this.panelWorkSpace.TabIndex = 1;
+            // 
+            // panelDrawBox
+            // 
+            this.panelDrawBox.BackColor = System.Drawing.Color.Transparent;
+            this.panelDrawBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.panelDrawBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelDrawBox.Location = new System.Drawing.Point(0, 0);
+            this.panelDrawBox.Name = "panelDrawBox";
+            this.panelDrawBox.Size = new System.Drawing.Size(626, 390);
+            this.panelDrawBox.TabIndex = 0;
+            this.panelDrawBox.Paint += new System.Windows.Forms.PaintEventHandler(this.panelDrawBox_Paint);
+            // 
+            // button1
+            // 
+            this.button1.Image = global::circles.Properties.Resources.save;
+            this.button1.Location = new System.Drawing.Point(637, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(32, 23);
+            this.button1.TabIndex = 7;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Circles
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(683, 487);
+            this.Controls.Add(this.numericUpDownScale);
+            this.Controls.Add(this.labelScale);
+            this.Controls.Add(this.panelWorkSpace);
+            this.Controls.Add(this.panelTools);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Circles";
+            this.Text = "Circles";
+            this.panelTools.ResumeLayout(false);
+            this.panelTools.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownIncrement)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownRadius)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownInterval)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownHeight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownWidth)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownScale)).EndInit();
+            this.panelWorkSpace.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelTools;
+        private System.Windows.Forms.Panel panelWorkSpace;
+        private System.Windows.Forms.Panel panelDrawBox;
+        private System.Windows.Forms.NumericUpDown numericUpDownIncrement;
+        private System.Windows.Forms.NumericUpDown numericUpDownRadius;
+        private System.Windows.Forms.NumericUpDown numericUpDownInterval;
+        private System.Windows.Forms.NumericUpDown numericUpDownHeight;
+        private System.Windows.Forms.NumericUpDown numericUpDownWidth;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonRefresh;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labelScale;
+        private System.Windows.Forms.NumericUpDown numericUpDownScale;
+    }
+}
+
---
+++ Circles2/circles/circles/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
+namespace circles
+{
+    public partial class Circles : Form
+    {
+        public Bitmap Bmp;
+        public double CanvasWidth;
+        public double CanvasHeight;
+        public double CanvasScale;
+
+
+
+        public Circles()
+        {
+            InitializeComponent();
+
+            numericUpDownWidth.Value = 600;
+            numericUpDownHeight.Value = 400;
+            numericUpDownIncrement.Value = 1;
+            numericUpDownInterval.Value = 20;
+            numericUpDownRadius.Value = 2;
+            numericUpDownScale.Value = 100;
+            Recalcuate();
+
+        }
+
+        private void OnValueChanged(object sender, EventArgs e)
+        {
+            Recalcuate();
+            panelDrawBox.Invalidate();
+        }
+
+        private void Recalcuate()
+        {
+            CanvasWidth = (double) numericUpDownWidth.Value;
+            CanvasHeight = (double) numericUpDownHeight.Value;
+            CanvasScale = (double) numericUpDownScale.Value;
+            panelDrawBox.Width = (int) (CanvasWidth * CanvasScale / 100);
+            panelDrawBox.Height = (int) (CanvasHeight * CanvasScale / 100);
+        }
+
+        private void panelDrawBox_Paint(object sender, PaintEventArgs e)
+        {
+            Bmp = new Bitmap((int)CanvasWidth, (int)CanvasHeight);
+            double up = 0;
+            double left = 0;
+            var down = CanvasHeight;
+            var right = CanvasWidth;
+            var radius = (double)numericUpDownRadius.Value;
+            var interval = (double) numericUpDownInterval.Value;
+            var increment = (double) numericUpDownIncrement.Value;
+            var width = CanvasWidth;
+            var height = CanvasHeight;
+
+            using (var g = Graphics.FromImage(Bmp))
+            {
+                
+                g.Clear(Color.Transparent);
+                if (interval == 0)
+                    return;
+
+                var circlesX = Math.Floor(width / interval);
+                var circlesY = Math.Floor(height / interval);
+
+                if (circlesX == 0 || circlesY == 0)
+                    return;
+
+                var trueIntervalX = width / circlesX;
+                var trueIntervalY = height / circlesY;
+
+                for(;;)
+                {
+                    up += trueIntervalY;
+                    down -= trueIntervalY;
+                    left += trueIntervalX;
+                    right -= trueIntervalX;
+
+                    width = right - left;
+                    height = down - up;
+
+                    // если центральные кружочки не помещаются, рисуем их ровно по центру с перекрытием
+                    if (width < 0)
+                    {
+                        if(-width > radius)
+                            break;
+
+                        left = (left + right) / 2;
+                        right = (left + right) / 2;
+                        
+
+                    }
+
+                    if (height < 0)
+                    {
+                        if (-height > radius)
+                            break;
+                        
+                        
+
+                        up = (up + down) / 2 - 1;
+                        down = (up + down) / 2 + 1;
+
+                    }
+
+                    var diameter = radius*2;
+
+
+                    for (var i = left; i <= right+1; i += trueIntervalX)
+                    {
+                        g.FillEllipse(Brushes.White, (int) (i - radius), (int) (up - radius), (int) diameter,
+                            (int) diameter);
+                        g.FillEllipse(Brushes.White, (int) (i - radius), (int) (down - radius), (int) diameter,
+                            (int) diameter);
+                    }
+
+                    for (var j = up; j <= down+1 ; j += trueIntervalY)
+                    {
+                        g.FillEllipse(Brushes.White, (int) (left - radius), (int) (j - radius), (int) diameter,
+                            (int) diameter);
+                        g.FillEllipse(Brushes.White, (int) (right - radius), (int) (j - radius), (int) diameter,
+                            (int) diameter);
+                    }
+
+                    if (radius + increment < right && radius + increment > 0)
+                        // круг не должен быть на весь экран или минусового размера
+                        radius += increment;
+                } 
+
+            }
+            var s = (int) numericUpDownScale.Value;
+            var destRect = new Rectangle(0, 0, (int)(CanvasWidth * s / 100), (int)(CanvasHeight * s/100));
+            var imgRect = new Rectangle(0, 0, (int)CanvasWidth, (int)CanvasHeight);
+            e.Graphics.InterpolationMode = InterpolationMode.NearestNeighbor;
+            e.Graphics.DrawImage(Bmp, destRect,imgRect, GraphicsUnit.Pixel);
+            
+        }
+
+        private void buttonRefresh_Click(object sender, EventArgs e)
+        {
+            panelDrawBox.Invalidate();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var sfd = new SaveFileDialog {Filter = "Файл PNG|*.png"};
+            var format = ImageFormat.Png;
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                Bmp.Save(sfd.FileName, format);
+            }
+            
+        }
+    }
+}
